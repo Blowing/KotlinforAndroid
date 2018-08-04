ffaf838eb00619b9adb92ecaa2711c703547f887
Blowing <1072878103@qq.com>
2018-08-04 17:26:18 +0800

    提交一个设置状态栏颜色的方法

--- app/src/main/java/com/wujie/kotlinforandroid/utils/Utils.kt
+++ app/src/main/java/com/wujie/kotlinforandroid/utils/Utils.kt
@@ -1,5 +1,11 @@
 package com.wujie.kotlinforandroid.utils
 
+import android.app.Activity
+import android.os.Build
+import android.support.annotation.ColorInt
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,6 +25,28 @@ class Utils {
                 result = true
             }
             return result
+        }
+
+
+
+        /**
+         * 设置状态栏颜色的公共方法
+         */
+        fun psetStatusBarBackgroundColor(activity: Activity, @ColorInt colorPref: Int, isLight: Boolean) {
+
+            if(Build.VERSION.SDK_INT >= 23) {
+                val window: Window = activity.window
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                if(isLight) {
+                    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+                } else {
+                    window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                }
+                window.statusBarColor = colorPref
+            }
+
         }
     }
 }
